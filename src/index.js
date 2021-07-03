1eb06ec7d098441b658fa953c3092294ace4e006
Angelo Martinez <amart670@fiu.edu>
2021-07-03 12:30:24 -0400

    finished

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 (() => {
   // iife to not pollute global scope
-  const headerLogo = document.querySelector('.js-team-logo');
+  const logos = document.querySelectorAll('.js-team-logo');
   const heroImg = document.querySelector('.js-hero-img');
-  const teamName = document.querySelectorAll('.js-team-name');
+  const teamNames = document.querySelectorAll('.js-team-name');
   const heroYear = document.querySelector('.js-year');
   const teamDescription = document.querySelector('.js-team-desc');
   const stadiumImg = document.querySelector('.js-stadium-img');
   const teamManager = document.querySelector('.js-manager');
   const teamStadium = document.querySelector('.js-stadium');
   const teamCapacity = document.querySelector('.js-stadium-cap');
+  const facebookLink = document.querySelector('.js-facebook');
+  const twitterLink = document.querySelector('.js-twitter');
+  const instagramLink = document.querySelector('.js-instagram');
 
   fetch(
     'https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=miami_marlins'
@@ -17,14 +20,17 @@
     .then((data) => {
       const teamObj = data.teams[0];
 
-      headerLogo.src = teamObj.strTeamLogo;
+      logos.forEach((node) => (node.src = teamObj.strTeamLogo));
       heroImg.style.backgroundImage = `url(${teamObj.strTeamFanart4})`;
-      teamName.forEach((node) => (node.innerText = teamObj.strTeam));
+      teamNames.forEach((node) => (node.innerText = teamObj.strTeam));
       heroYear.innerText = teamObj.intFormedYear;
       teamDescription.innerText = teamObj.strDescriptionEN;
       stadiumImg.style.backgroundImage = `url(${teamObj.strStadiumThumb})`;
       teamManager.innerText = teamObj.strManager;
       teamStadium.innerText = teamObj.strStadium;
       teamCapacity.innerText = teamObj.intStadiumCapacity;
+      facebookLink.href = `https://${teamObj.strFacebook}`;
+      twitterLink.href = `https://${teamObj.strTwitter}`;
+      instagramLink.href = `https://${teamObj.strInstagram}`;
     });
 })();
