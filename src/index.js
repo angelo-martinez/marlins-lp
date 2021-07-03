570f385cd03e5bc7e3b109b6e817c931a33081a7
Angelo Martinez <amart670@fiu.edu>
2021-07-03 11:52:40 -0400

    added fun facts

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@
   // iife to not pollute global scope
   const headerLogo = document.querySelector('.js-team-logo');
   const heroImg = document.querySelector('.js-hero-img');
-  const heroTeamName = document.querySelector('.js-team-name');
+  const teamName = document.querySelectorAll('.js-team-name');
   const heroYear = document.querySelector('.js-year');
+  const teamDescription = document.querySelector('.js-team-desc');
+  const stadiumImg = document.querySelector('.js-stadium-img');
+  const teamManager = document.querySelector('.js-manager');
+  const teamStadium = document.querySelector('.js-stadium');
+  const teamCapacity = document.querySelector('.js-stadium-cap');
 
   fetch(
     'https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=miami_marlins'
@@ -11,9 +16,15 @@
     .then((response) => response.json())
     .then((data) => {
       const teamObj = data.teams[0];
+
       headerLogo.src = teamObj.strTeamLogo;
       heroImg.style.backgroundImage = `url(${teamObj.strTeamFanart4})`;
-      heroTeamName.innerText = teamObj.strTeam;
+      teamName.forEach((node) => (node.innerText = teamObj.strTeam));
       heroYear.innerText = teamObj.intFormedYear;
+      teamDescription.innerText = teamObj.strDescriptionEN;
+      stadiumImg.style.backgroundImage = `url(${teamObj.strStadiumThumb})`;
+      teamManager.innerText = teamObj.strManager;
+      teamStadium.innerText = teamObj.strStadium;
+      teamCapacity.innerText = teamObj.intStadiumCapacity;
     });
 })();
